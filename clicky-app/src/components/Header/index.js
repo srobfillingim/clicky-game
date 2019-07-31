060aef2a9aecdcbfec085737d332208678fc4acd
Samuel Fillingim <srobfillingim@gmail.com>
2019-07-30 23:00:03 -0500

    style, header, nav, nav message

--- clicky-app/src/components/Header/index.js
+++ clicky-app/src/components/Header/index.js
@@ -1,3 +1,13 @@
 import React from "react";
 import "./style.css";
 
+function Header() {
+  return (
+    <header className="header">
+      <h1>Clicky Game!</h1>
+      <h2>Click on an image to earn points, reapeat an image and you lose a point.</h2>
+    </header>
+  );
+}
+
+export default Header;
