c9e7125ea1d59be9ff8fd0dd4ddb141cb555e7bc
Samuel Fillingim <srobfillingim@gmail.com>
2019-07-30 22:49:15 -0500

    clickItem, container, footer

--- clicky-app/src/components/Footer/index.js
+++ clicky-app/src/components/Footer/index.js
@@ -1,3 +1,14 @@
 import React from "react";
 import "./style.css";
 
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="bottom">
+        Samuel Fillingim 2019
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
